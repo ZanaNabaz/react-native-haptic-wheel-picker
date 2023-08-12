e50bb11f9c6ee402feab6b6aaa91ca83840313c5
Zana Nabaz <39528296+ZanaNabaz@users.noreply.github.com>
2023-08-12 17:54:58 +0200

    Update PickerItem.tsx
    Adding horizontal props
    isHorizontal : boolean

--- src/PickerItem.tsx
+++ src/PickerItem.tsx
@@ -1,118 +1,142 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TextStyle} from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextStyle } from "react-native";
 import Animated, {
-  useAnimatedStyle,
-  useDerivedValue,
-} from 'react-native-reanimated';
+	useAnimatedStyle,
+	useDerivedValue,
+} from "react-native-reanimated";
 
 interface PickerItemProps<T> {
-  item: T;
-  index: number;
-  translateY: {value: number};
-  itemHeight: number;
-  textStyle?: TextStyle;
-  renderItem?: (item: T) => JSX.Element;
-  itemDistanceMultipier: number;
-  wheelHeightMultiplier: number;
+	item: T;
+	index: number;
+	translateY: { value: number };
+	translateX: { value: number };
+	itemHeight: number;
+	itemWidth: number;
+	textStyle?: TextStyle;
+	renderItem?: (item: T) => JSX.Element;
+	itemDistanceMultipier: number;
+	wheelHeightMultiplier: number;
+	isHorizontal: boolean;
 }
 
 const calcValue = (
-  translateY: number,
-  offset: number,
-  itemHeight: number,
-  itemDistanceMultipier: number,
-) => {
-  'worklet';
+	translateValue: number,
 
-  return Math.sin(
-    Math.max(
-      Math.min(
-        (translateY + offset) / (itemHeight / itemDistanceMultipier),
-        Math.PI * 0.5,
-      ),
-      -Math.PI * 0.5,
-    ),
-  );
+	offset: number,
+	itemSize: number,
+	itemDistanceMultipier: number,
+) => {
+	"worklet";
+
+	return Math.sin(
+		Math.max(
+			Math.min(
+				(translateValue + offset) / (itemSize / itemDistanceMultipier),
+				Math.PI * 0.5,
+			),
+			-Math.PI * 0.5,
+		),
+	);
 };
 
 const PickerItem = <T,>({
-  item,
-  index,
-  translateY,
-  itemHeight,
-  textStyle,
-  renderItem,
-  itemDistanceMultipier,
-  wheelHeightMultiplier,
+	item,
+	index,
+	translateY,
+	translateX,
+	itemHeight,
+	itemWidth,
+	textStyle,
+	renderItem,
+	itemDistanceMultipier,
+	wheelHeightMultiplier,
+	isHorizontal,
 }: PickerItemProps<T>) => {
-  const [width, setWidth] = useState(0);
-  const offset = index * itemHeight;
-  const value = useDerivedValue(() =>
-    calcValue(translateY.value, offset, itemHeight, itemDistanceMultipier),
-  );
+	const [width, setWidth] = useState(0);
+	const offset = index * itemHeight;
+	const value = useDerivedValue(() =>
+		isHorizontal
+			? calcValue(translateX.value, offset, itemWidth, itemDistanceMultipier)
+			: calcValue(translateY.value, offset, itemHeight, itemDistanceMultipier),
+	);
 
-  const animatedStyleWheelTranslation = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: value.value * itemHeight * wheelHeightMultiplier,
-        },
-      ],
-      opacity: 1 - Math.abs(value.value),
-      height: itemHeight,
-    };
-  });
+	const animatedStyleWheelTranslation = useAnimatedStyle(() => {
+		return {
+			transform: [
+				isHorizontal
+					? {
+							translateX: value.value * itemWidth * wheelHeightMultiplier,
+					  }
+					: {
+							translateY: value.value * itemHeight * wheelHeightMultiplier,
+					  },
+			],
+			opacity: 1 - Math.abs(value.value),
+			height: itemWidth,
+		};
+	});
 
-  const animatedStyleScaling = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          scaleY: 1 - Math.abs(value.value),
-        },
-      ],
-    };
-  });
+	const animatedStyleScaling = useAnimatedStyle(() => {
+		return {
+			transform: [
+				isHorizontal
+					? {
+							scaleX: 1 - Math.abs(value.value),
+					  }
+					: {
+							scaleY: 1 - Math.abs(value.value),
+					  },
+			],
+		};
+	});
 
-  return (
-    <Animated.View
-      onLayout={(event) => {
-        if (event.nativeEvent.layout.width > width) {
-          setWidth(event.nativeEvent.layout.width);
-        }
-      }}
-      style={[styles.item, animatedStyleWheelTranslation]}>
-      <Animated.View style={[styles.itemInner, animatedStyleScaling]}>
-        {renderItem ? (
-          renderItem(item)
-        ) : (
-          <Text style={[styles.itemText, textStyle]}>{`${item}`}</Text>
-        )}
-      </Animated.View>
-    </Animated.View>
-  );
+	return (
+		<Animated.View
+			onLayout={(event) => {
+				if (event.nativeEvent.layout.width > width) {
+					setWidth(event.nativeEvent.layout.width);
+				}
+			}}
+			style={[
+				isHorizontal === true ? styles.item_horizontal : styles.item,
+				animatedStyleWheelTranslation,
+			]}
+		>
+			<Animated.View style={[styles.itemInner, animatedStyleScaling]}>
+				{renderItem ? (
+					renderItem(item)
+				) : (
+					<Text style={[styles.itemText, textStyle]}>{`${item}`}</Text>
+				)}
+			</Animated.View>
+		</Animated.View>
+	);
 };
 
 PickerItem.defaultProps = {
-  renderItem: undefined,
-  textStyle: undefined,
+	renderItem: undefined,
+	textStyle: undefined,
 };
 
 const styles = StyleSheet.create({
-  item: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-  },
-  itemInner: {
-    width: '100%',
-  },
-  itemText: {
-    color: 'white',
-    paddingHorizontal: 16,
-    textAlign: 'center',
-  },
+	item: {
+		alignItems: "center",
+		justifyContent: "center",
+		position: "absolute",
+		top: 0,
+		width: "100%",
+	},
+	item_horizontal: {
+		position: "absolute",
+	},
+	itemInner: {
+		width: "100%",
+	},
+	itemText: {
+		color: "white",
+		paddingHorizontal: 16,
+		textAlign: "center",
+	},
 });
 
 export default PickerItem;
